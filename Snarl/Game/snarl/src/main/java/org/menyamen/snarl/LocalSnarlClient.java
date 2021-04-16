9774817377d113eded003dd87c3e44dab1b596ee
manvi27 <tandon.m@northeastern.edu>
2021-04-16 11:37:57 -0400

    datatoSend as UTF, bug fixes

--- Snarl/Game/snarl/src/main/java/org/menyamen/snarl/LocalSnarlClient.java
+++ Snarl/Game/snarl/src/main/java/org/menyamen/snarl/LocalSnarlClient.java
@@ -1,29 +1,18 @@
 package org.menyamen.snarl;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import org.menyamen.snarl.characters.PlayerImpl;
 
-public class LocalSnarlClient {
+import java.io.*;
+import java.util.Scanner;
+import java.io.IOException;
+import java.net.Socket;
+  
+// Client class
+public class LocalSnarlClient 
+{
     private static String address = "127.0.0.1";
     private static int port = 4568;
-    private static String session;
-    private static int playerCount = 3;
-
-    public LocalSnarlClient() {
-    }
-
-    /**
-     * Main method.
-     * @param args The arguments of the program.
-     */
-    public static void main(String[] args) {
-
+  
+    public static void main(String[] args) throws IOException 
+    {
         for (int i = 0; i < args.length; i++) {
             if(args[i].equals("--address")) {
                 address = (args[i + 1]);
@@ -32,40 +21,45 @@ public class LocalSnarlClient {
                 port = Integer.parseInt(args[i + 1]);
             }
         }
-
-        try {
-            // Startup
-
-            // Create a connection with port
-            Socket sock = new Socket(address, port);
-            System.out.println("Connected!");
-            // Open input/output streams
-            //PrintStream outStream = new PrintStream(sock.getOutputStream());
-            //BufferedReader inStream = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-            OutputStream outputStream = sock.getOutputStream();
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
-
-            // [sign-up players]
-            List<PlayerImpl> players = new ArrayList<PlayerImpl>();
-            Scanner input = new Scanner(System.in);
-
-            for (int i = 0; i < playerCount; i++) {
-                System.out.print("Enter Player " + (i + 1) + " Name: ");
-                String name = input.nextLine();
-                players.add(new PlayerImpl(name));
+        
+        try
+        {
+            Scanner scn = new Scanner(System.in);
+              
+            // Create a connection with server
+            Socket s = new Socket(address, port);
+            System.out.println("Connected to server on: " + "adrress=" + address + " port=" + port);
+      
+            // obtaining input and out streams
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+      
+            // the following loop performs the exchange of
+            // information between client and server
+            while (true) 
+            {
+                System.out.println(dis.readUTF());
+                String dataToSend = scn.nextLine();
+                 
+                // If client sends exit,close this connection 
+                // and then break from the while loop
+                if(dataToSend.equals("Exit"))
+                {
+                    System.out.println("Closing this connection : " + s);
+                    s.close();
+                    System.out.println("Connection closed");
+                    break;
+                }
+                  
+                dos.writeUTF(dataToSend);
+             
             }
-
-            objectOutputStream.writeObject(players);
-            input.close();
-            sock.close();
-            
-        } catch (UnknownHostException ex) {
-     
-            System.out.println("Server not found: " + ex.getMessage());
-     
-        } catch (IOException ex) {
-     
-            System.out.println("I/O error: " + ex.getMessage());
+            // closing resources
+            scn.close();
+            dis.close();
+            dos.close();
+        }catch(Exception e){
+            e.printStackTrace();
         }
     }
 }
