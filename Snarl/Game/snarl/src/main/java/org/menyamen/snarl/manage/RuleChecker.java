e0f4c2d2caee30d3abe824273743eb58407dd04b
manvi27 <tandon.m@northeastern.edu>
2021-04-21 12:35:38 -0400

    move check for adversaries

--- Snarl/Game/snarl/src/main/java/org/menyamen/snarl/manage/RuleChecker.java
+++ Snarl/Game/snarl/src/main/java/org/menyamen/snarl/manage/RuleChecker.java
@@ -4,6 +4,8 @@ import java.awt.Point;
 import java.util.List;
 
 import org.menyamen.snarl.characters.Player;
+import org.menyamen.snarl.characters.RemoteAdversary;
+import org.menyamen.snarl.constraints.CharacterEnum;
 import org.menyamen.snarl.constraints.MoveResult;
 import org.menyamen.snarl.gameobjects.GameObject;
 import org.menyamen.snarl.gameobjects.GameObject.GameObjectType;
@@ -55,6 +57,25 @@ public class RuleChecker {
         
     }
 
+    //Move Check used by game manager 
+    protected MoveResult moveCheckAdversary(FullState state, RemoteAdversary adversary, Point end) throws IllegalArgumentException {
+        Level level = state.getCurrentLevel();
+        List<Point> possibleMoves = level.cardinalMove(adversary.getPos(), 1);
+        if (!possibleMoves.contains(end)) {
+            return MoveResult.NOTTRAVERSABLE;
+        }
+        if (level.isOccupiedBy(end) == CharacterEnum.ADVERSARY) {
+            return MoveResult.INVALID;
+        }
+        if (adversary.getType().equals("zombie") && !level.isTraversable(end)) {
+            return MoveResult.NOTTRAVERSABLE;
+        }
+        
+        return MoveResult.SUCCESS;
+        
+    }
+
+    //Game over check 
     protected boolean gameOverCheck(FullState state, int turns) {
         if (turns == 0) {
             return true;
